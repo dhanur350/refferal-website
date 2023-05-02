65c1ccda7b21bdac7e977372a427f8d8a42c3e90
dhanur350 <dhanur297@gmail.com>
2023-05-03 01:41:34 +0530

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,35 @@
-function getInterest(){
-    let selectedValues = document.getElementById("interest");
-    let text = selectedValues.options[selectedValues.selectedIndex].text;
-    console.log(text);
+// function getInterest(){
+    
+// }//
+
+function myfunction(){
+    console.log("form submitted");
     let formref = document.getElementById('formref');
     formref.addEventListener('submit', (event)=>{
-        console.log(event);
+        let selectedValues = document.getElementById("interest");
+    let text = selectedValues.options[selectedValues.selectedIndex].text;
+    console.log(text);
         event.preventDefault();
+    console.log(formref);
+        console.log(true);
         if(text === "Non-Medical"){
-            windows.location.href("/src/nonmedical.html");
+            console.log(true);
+            window.location.href ="/src/nonmedical.html";
+        }
+        if(text === "Medical"){
+            console.log(true);
+            window.location.href ="/src/medical.html";
+        }
+        if(text === "Commerce"){
+            console.log(true);
+            window.location.href ="/src/commerce.html";
+        }
+        if(text === "Humanity"){
+            console.log(true);
+            window.location.href ="/src/humanity.html";
+        }
+        else{
+            console.log(false);
         }
     })
-}//
+}
