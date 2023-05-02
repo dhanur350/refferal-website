d677c0dc40a517184c6c53f390af701815d2cb9d
dhanur350 <dhanur297@gmail.com>
2023-05-02 23:50:11 +0530

    Homepage completed

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+function getInterest(){
+    let selectedValues = document.getElementById("interest");
+    let text = selectedValues.options[selectedValues.selectedIndex].text;
+    console.log(text);
+    let formref = document.getElementById('formref');
+    formref.addEventListener('submit', (event)=>{
+        console.log(event);
+        event.preventDefault();
+        if(text === "Non-Medical"){
+            windows.location.href("/src/nonmedical.html");
+        }
+    })
+}
